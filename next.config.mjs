a491447280a5420c8e362a27c401e823184343d0
Rituraj <deepikanegi2309@gmail.com>
2024-11-19 02:41:31 +0530

    Fixed Suspense Error

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      missingSuspenseWithCSRBailout: false,
-    },
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'ap-south-1.graphassets.com',
-          port: '',
-          pathname: '/**',
-        },
-        {
-          protocol: 'https',
-          hostname: 'img.clerk.com',
-          port: '',
-          pathname: '/**',
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+    images:{
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'ap-south-1.graphassets.com',
+                port: '',
+                pathname: '/**'
+            },
+            {
+                protocol: 'https',
+                hostname: 'img.clerk.com',
+                port: '',
+                pathname: '/**'
+            },
+        ]
+    }
+};
+
+export default nextConfig;
